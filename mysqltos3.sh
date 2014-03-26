8eb917a1ceff6df79dc268fcdabf807d72e7e9ab
Alika Larsen <alika.larsen@atsid.com>
2014-03-25 17:28:20 -0700

    s3cmd not doing delete on move. Adding timing info.

--- mysqltos3.sh
+++ mysqltos3.sh
@@ -13,6 +13,7 @@ DATABASE='--all-databases'
 S3PATH=
 # when running via cron, the PATHs MIGHT be different. If you have a custom/manual MYSQL install, you should set this manually like MYSQLDUMPPATH=/usr/local/mysql/bin/
 MYSQLDUMPPATH=
+S3CMDPATH=~/s3cmd/location/
 #tmp path.
 TMP_PATH=/tmp/
 
@@ -44,17 +45,20 @@ else
 	ROTATE=${ROTATE_DAY}
 fi
 
+STARTTIME="$(date +%s%N)"
+
 echo "Rotating ${PERIOD} backups (${ROTATE})."
 
 for num in $(seq $ROTATE -1 1) 
 do
 	if [ ${ROTATE} = ${num} ]; then
 		echo "\tRemoving oldest backup (${ROTATE} ${PERIOD}s ago)..."
-		s3cmd del --recursive s3://${S3BUCKET}/${S3PATH}${PERIOD}/${num}/
+		${S3CMDPATH}s3cmd del --recursive s3://${S3BUCKET}/${S3PATH}${PERIOD}/${num}/
 		echo "\tOldest backup removed."
 	else
 		echo "\tMoving the backup from past ${num} ${PERIOD}s to $((num+1))"
-		s3cmd mv --recursive s3://${S3BUCKET}/${S3PATH}${PERIOD}/${num}/ s3://${S3BUCKET}/${S3PATH}${PERIOD}/$((num+1))/
+		${S3CMDPATH}s3cmd mv --recursive s3://${S3BUCKET}/${S3PATH}${PERIOD}/${num}/ s3://${S3BUCKET}/${S3PATH}${PERIOD}/$((num+1))/
+		${S3CMDPATH}s3cmd del --recursive s3://${S3BUCKET}/${S3PATH}${PERIOD}/${num}/ 
 		echo "\tPast backup moved."
 	fi
 done
@@ -73,7 +77,7 @@ echo "Done compressing the backup file."
 
 # upload backup (s3cmd supports pre-upload encryption: -e)
 echo "Uploading the new backup..."
-s3cmd put -f ${TMP_PATH}${FILENAME}${DATESTAMP}.tar.gz s3://${S3BUCKET}/${S3PATH}${PERIOD}/1/
+${S3CMDPATH}s3cmd put -f ${TMP_PATH}${FILENAME}${DATESTAMP}.tar.gz s3://${S3BUCKET}/${S3PATH}${PERIOD}/1/
 echo "New backup uploaded."
 
 echo "Removing the cache files..."
@@ -82,3 +86,7 @@ rm ${TMP_PATH}${FILENAME}.sql
 rm ${TMP_PATH}${FILENAME}${DATESTAMP}.tar.gz
 echo "Files removed."
 echo "All done."
+
+ENDTIME="$(($(date +%s%N)-STARTTIME))"
+S="$((ENDTIME/1000000000))"
+printf "Timing: Days:%02d Hours:%02d Minutes:%02d Seconds:%02d.%03d\n" "$((S/86400))" "$((S/3600%24))" "$((S/60%60))" "$((S%60))" "${M}"
